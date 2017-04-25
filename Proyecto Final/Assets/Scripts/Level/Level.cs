497254f0768808805ac7853cfa21ddad8bd75708
itsStrobe <josezavala0297@gmail.com>
2017-04-25 03:43:43 -0500

    Primer Avance

---
+++ Proyecto Final/Assets/Scripts/Level/Level.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+	[Serializable]
+	public class Count
+	{
+		public int minimum;
+		public int maximum;
+
+		public Count(int min, int max)
+		{
+			minimum = min;
+			maximum = max;
+		}
+	}
+
+	public int columns = 8;
+	public int rows = 8;
+	public Count wallCount = new Count (5, 9);
+	public Count foodCount = new Count (1, 5);
+	public GameObject exit;
+	public GameObject[] floorTiles;
+	public GameObject[] wallTiles;
+
+	private Transform boardHoalder;
+	private List<Vector3> gridPositions = new List<Vector3> ();
+
+	void InitialiseList()
+	{
+		gridPositions.Clear ();
+		for (int x = 1; x < columns - 1; x++) {
+			for (int y = 1; y < rows - 1; y++) {
+				gridPositions.Add (new Vector3 (x, y, 0f));
+
+			}
+		}
+	}
+
+	void LevelSetup(){
+		boardHoalder = new GameObject ("Board").transform;
+
+		for (int x = -1; x < columns + 1; x++) {
+			for (int y = -1; y < rows + 1; y++) {
+				
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ Proyecto Final/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// PlayerScript requires the GameObject to have a Rigidbody2D component
+
+[RequireComponent (typeof (Rigidbody2D))]
+
+public class PlayerMovement : MonoBehaviour {
+
+
+	public float playerSpeed = 4f;
+	private Vector2 sPosition;
+
+	void Start () {
+		sPosition = transform.position;
+	}
+
+	void FixedUpdate () {
+		Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		GetComponent<Rigidbody2D>().velocity=targetVelocity * playerSpeed;
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.CompareTag ("FOV")) {
+			transform.position = sPosition;
+			print ("Hi");
+		}
+			
+	}
+}
