71a5ee36bb5c383c22e62c9a7ac7c146e7681d56
Carlos Lopez <carloslpz182@gmail.com>
2017-04-29 21:50:51 -0500

    Makes corrections to cop movement script

--- Proyecto Final/Assets/Scripts/Cop/CopMovement.cs
+++ Proyecto Final/Assets/Scripts/Cop/CopMovement.cs
@@ -23,7 +23,7 @@ public class CopMovement : MonoBehaviour {
 
 		GetComponent<Rigidbody2D>().velocity = direction * speed;
 
-		switch (movementOrder [currentMovement]) {
+		switch (movementOrder [currentMovement%arraySize]) {
 		case 0:
 			if ((transform.position.y - initialPosition.y) > dBound) {
 				nextMove = currentMovement;
@@ -54,7 +54,7 @@ public class CopMovement : MonoBehaviour {
 		initialPosition = transform.position;
 		currentMovement = 0;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
@@ -62,11 +62,11 @@ public class CopMovement : MonoBehaviour {
 		 * 1 -> Up
 		 * 2 -> Left
 		 * 3 -> Rigth
-		 */ 
+		 */
 		int arraySize = movementOrder.Length;
 		Vector2 direction = new Vector2 (0, 0);
 
-		switch (movementOrder [currentMovement]) {
+		switch (movementOrder [currentMovement%arraySize]) {
 		case 0:
 			direction.x = 0;
 			direction.y = -1;
